ebb3407ae72020cab60be3ec13e729b201fba222
Maciej Jur <maciej@kamoshi.org>
2022-12-01 14:41:31 +0100

    Add photo-rename CLI options

--- photo-rename/src/main.rs
+++ photo-rename/src/main.rs
@@ -1,8 +1,28 @@
 mod photo_rename;
 
+use clap::Parser;
+
+
+/// CLI util for mass renaming photos
+#[derive(Parser, Debug)]
+struct Args {
+    /// Path to the input directory
+    #[arg(short, long)]
+    input: String,
+
+    /// Path to the output directory
+    #[arg(short, long)]
+    output: String,
+}
+
 
 fn main() {
-    let input = "./in/";
-    let output = "./out/";
-    photo_rename::run_for(input, output).expect("TODO: panic message");
+    let args = Args::parse();
+
+    let input: String = args.input;
+    let output: String = args.output;
+    match photo_rename::run_for(input.as_str(), output.as_str()) {
+        Ok(_) => println!("Task finished!"),
+        Err(err) => eprintln!("{}", err),
+    }
 }
